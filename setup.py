08f027c2111c9cf7b70beaa218915410da5df29b
evowilliamson <ivo_willemsen@hotmail.com>
2019-11-20 13:48:39 +0100

    Setuptools changes

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+setup(name="python-algorithms-datastructures",
+      version=0.1,
+      description="A set of algorithms and data structures",
+      long_description=long_desc,
+      long_description_content_type="text/markdown", 
+      keywords = "data structures algorithms graphs directed graphs acyclic",
+      author="Evo Williamson",
+      author_email="evowilliamson@gmail.com",
+      license="GNU General Public License, version 2",
+      url="https://github.com/evowilliamson/python-algorithms-datastructures",
+      packages=find_packages(),
+      install_requires=[
+          "graphviz",
+      ],
+      test_suite="tests",
+      classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Software Development",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent"
+        ,
+      ]
+     )
