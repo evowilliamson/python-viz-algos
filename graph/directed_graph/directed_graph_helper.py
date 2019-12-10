523318cf614d52eaee97cfe5432f3117b9bd0ef4
evowilliamson <ivo_willemsen@hotmail.com>
2019-12-10 15:21:10 +0100

    Removed logging from cycle checking

--- graph/directed_graph/directed_graph_helper.py
+++ graph/directed_graph/directed_graph_helper.py
@@ -122,11 +122,8 @@ def is_cyclic_dfs(directed_graph, vertex, traversed_already, in_cycle):
     in_cycle[vertex.get_label()] = True
     viztrace_visit_tail(directed_graph, vertex)
 
-    Logging.inc_indent()
     for head in vertex.get_heads():
-        Logging.log("Vertex {0}, head {1}", vertex.get_label(), head.get_label())
         if traversed_already.get(head.get_label()) is None:
-            Logging.log("Head {0} not yet traversed", head.get_label())
             if is_cyclic_dfs(directed_graph, head, traversed_already, in_cycle):
                 viztrace_log_cycle_reported(directed_graph, head)
                 return True
@@ -136,11 +133,8 @@ def is_cyclic_dfs(directed_graph, vertex, traversed_already, in_cycle):
         elif in_cycle[head.get_label()]:
             viztrace_log_cycle_found(directed_graph, vertex, head)
             return True
-        elif traversed_already.get(head.get_label()):
-            Logging.log("Head {0} traversed already", head.get_label())
 
     in_cycle[vertex.get_label()] = False
-    # viztrace_log_finish_dfs(directed_graph, vertex)
     return False
 
 
@@ -166,15 +160,7 @@ def is_cyclic(directed_graph):
     return False
 
 
-def viztrace_log_activated_vertex(directed_graph, vertex):
-    Logging.log("Vertex {0}", vertex.get_label())
-    VizTracing.change_activated_vertex(directed_graph, vertex)
-    VizTracing.snapshot()
-
-
 def viztrace_log_cycle_reported(directed_graph, vertex):
-    Logging.log("Vertex {0} just reported a cyclic", vertex.get_label())
-    Logging.dec_indent()
     VizTracing.set_status(directed_graph, vertex, VizTracing.IN_CYCLE)
     VizTracing.change_activated_vertex(directed_graph, vertex)    
     VizTracing.snapshot()
@@ -187,15 +173,7 @@ def viztrace_visit_tail(directed_graph, vertex):
 
 
 def viztrace_log_cycle_found(directed_graph, tail, head):
-    Logging.log("Vertex {0}, head {1} cycle just found", tail.get_label(), head.get_label())
-    Logging.dec_indent()
     VizTracing.set_status(directed_graph, head, VizTracing.IN_CYCLE)
     VizTracing.change_activated_vertex(directed_graph, head)    
     VizTracing.snapshot()
 
-
-def viztrace_log_finish_dfs(directed_graph, vertex):
-    Logging.dec_indent()
-    if len(vertex.get_heads()) != 0:
-        VizTracing.change_activated_vertex(directed_graph, vertex)
-        VizTracing.snapshot()
