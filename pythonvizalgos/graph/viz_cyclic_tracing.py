63bca3c85288eee13fe91c02e5fd83162e65f480
evowilliamson <14161480+evowilliamson@users.noreply.github.com>
2020-05-23 18:24:06 +0200

    Working version

--- pythonvizalgos/graph/viz_cyclic_tracing.py
+++ pythonvizalgos/graph/viz_cyclic_tracing.py
@@ -1,11 +1,10 @@
 from graphviz import Digraph
 from pythonalgos.graph.vertex import Vertex
+from pythonalgos.util.advisor import Advisor
 from os import path
 from pythonalgos.util import path_tools as pt
 from pythonvizalgos.util import video_tools as vt
 from pythonalgos.graph.directed_graph import DirectedGraph
-from pythonvizalgos.main_runners.graph.advisors.viz_tracing_advisor \
-    import VizCyclicTracingAdvisor
 import os
 
 
@@ -210,3 +209,86 @@ class VizCyclicTracing:
             edge_states=[{VizCyclicTracing.DISABLED: {"color": "red"}}])
         directed_graph.is_cyclic(VizCyclicTracingAdvisor())
         vt.convert_images_to_video(pt.get_dir_in_user_home(work_path))
+
+
+class VizCyclicTracingAdvisor(Advisor):
+    """ Module that contains the logic for inserting advice at join points for
+    visualization of the cyclic check algorithm
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def cycle_reported_recursive(self, directed_graph, vertex):
+        """ Function that is used along the way back from the origin
+        of the cycle detection to the initial state. Along the way,
+        all vertices are tagged with the state in_cycle
+
+        Args:
+            directed_graph (DirectedGraph): The directed graph
+            vertex: the vertex that should get the status activated
+
+        """
+
+        VizCyclicTracing.set_status(
+            directed_graph, vertex, VizCyclicTracing.IN_CYCLE)
+        VizCyclicTracing.change_activated_vertex(directed_graph, vertex)
+        VizCyclicTracing.snapshot()
+
+    def visit_vertex(self, directed_graph, vertex):
+        """ Function that is used to tag vertices with the state "visisted",
+        if these vertices have been visited once. So next time, when another
+        predecessor of a tagged vertex is being considered, it is skipped
+
+        Args:
+            directed_graph (DirectedGraph): The directed graph
+            vertex: the vertex that should get the status "visited"
+
+        """
+        VizCyclicTracing.change_activated_vertex(directed_graph, vertex)
+        VizCyclicTracing.set_status(
+            directed_graph, vertex, VizCyclicTracing.VISISTED)
+        VizCyclicTracing.snapshot()
+
+    def cycle_found(self, directed_graph, tail, head):
+        """ Changes the state of a vertex when the vertex is part of a cycle
+
+        Args:
+            directed_graph (DirectedGraph): The directed graph
+            tail: the tail vertex that should get the status activated
+            head: the head vertex that should get the in_cycle status
+
+        """
+
+        VizCyclicTracing.set_status(
+            directed_graph, head, VizCyclicTracing.IN_CYCLE)
+        VizCyclicTracing.change_activated_vertex(directed_graph, head)
+        VizCyclicTracing.snapshot()
+
+    def no_cycle_reported_recursive(self, directed_graph, vertex):
+        """ Changes focus to the vertex and takes a snapshot
+
+        Args:
+            directed_graph(DirectedGraph): The directed graph
+            vertex: the vertex that should get the status activated
+
+        """
+
+        VizCyclicTracing.change_activated_vertex(directed_graph, vertex)
+        VizCyclicTracing.snapshot()
+
+    def vertex_already_visited(self, directed_graph, edge):
+        """ Function that takes a snapshot after having disabled the
+        edge. This is to indicate that the transition cannot be taken
+
+        Args:
+            directed_graph(DirectedGraph): The directed graph
+            edge(Edge): the edge to be disabled
+        """
+
+        VizCyclicTracing.set_status(
+            directed_graph, edge, VizCyclicTracing.DISABLED)
+        VizCyclicTracing.snapshot()
+        VizCyclicTracing.reset_status(
+            directed_graph, edge, VizCyclicTracing.DISABLED)
+        VizCyclicTracing.snapshot()
